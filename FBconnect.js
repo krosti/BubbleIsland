9bd8f5d49050e1b1ec37a59f9672c1fc4654c0e8
jcapoduri <jcapoduri@gmail.com>
2011-05-08 22:16:44 -0300

    nada

--- FBconnect.js
+++ FBconnect.js
@@ -108,8 +108,8 @@ function FBConnect(elementId){
 			url: 'https://graph.facebook.com/me',
 			type: 'GET',
 			data: postdata,
-			//success: function(data){ alert('success: ' + data); },
-			success: FBConnect.setUserData,
+			success: function(data){ alert('success: ' + data); FBConnect(data); },
+			//success: FBConnect.setUserData,
 			error: function(data, er, r){ alert(data.responseText + ':' + er + ':' +r); }
 		});
 	};

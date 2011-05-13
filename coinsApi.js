4f70ff91439494ec276417ca32164dee5dec0c39
jcapoduri <jcapoduri@gmail.com>
2011-05-13 01:40:36 -0300

    coins

--- coinsApi.js
+++ coinsApi.js
@@ -85,7 +85,7 @@ softgameApi.prototype.connectionEstablished = function(data){
 		this.jq = $.ajax({
 			type: 'GET',
 			url: faceuri,
-			success: function(data){ softgame.facebookConnectResponse(data) },
+			success: function(data){ softgame.facebookConnectResponse(data); alert(this.url); alert(this.jq.getAllResponseHeaders()); },
 			error: function(data, error){ alert('error first attemp'); }
 		});
 	}else{
@@ -99,7 +99,6 @@ softgameApi.prototype.connectionEstablished = function(data){
 
 softgameApi.prototype.facebookConnectResponse = function(data){
 	alert('facebookConnectResponse: ' + data);
-	alert('url: ' + this.jq.getResponseHeader("Location"));
 	if(data.indexOf('window.location.href="') != -1){
 		var faceuri = data.slice(data.indexOf('window.location.href="') + 22, data.indexOf('";'));
 		faceuri = '"' + faceuri + '"';
@@ -119,9 +118,9 @@ softgameApi.prototype.facebookConnectResponse = function(data){
 	};
 };
 
-softgameApi.prototype.secondConnectResponse = function(data, uri){
+softgameApi.prototype.secondConnectResponse = function(data){
 	alert('secondConnectResponse: ' + data);
-	alert('get url: ' + uri);
+	alert('url: ' + this.jq.getAllResponseHeaders());
 	this.element.style.display = 'block';
 	this.element.innerHTML = data;	
 	if(data.indexOf('window.location.href="') != -1){

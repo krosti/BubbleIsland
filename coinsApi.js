132b597abc56f7858dd0a3546d79d3a5e98918ea
jcapoduri <jcapoduri@gmail.com>
2011-05-13 02:19:45 -0300

    coins

--- coinsApi.js
+++ coinsApi.js
@@ -120,7 +120,7 @@ softgameApi.prototype.facebookConnectResponse = function(data){
 
 softgameApi.prototype.secondConnectResponse = function(data, uri){
 	alert('secondConnectResponse: ' + data);
-	alert('url: ' + uri + ' url: ' + this.url;
+	alert('url: ' + uri + ' url: ' + this.url);
 	/*this.element.style.display = 'block';
 	this.element.innerHTML = data;	
 	if(data.indexOf('window.location.href="') != -1){

866c690dee84321648e6037136bb3b3ebad9b7e2
jcapoduri <jcapoduri@gmail.com>
2011-05-25 05:11:52 -0300

    test

---
+++ api.js
@@ -0,0 +1,73 @@
+api = {};
+
+api.facebook = {};
+api.softgame = {};
+
+//Facebook api
+api.facebook.appid = '213255638692367';
+api.facebook.appkey = '230bd48e2d38a991fe41e284887db3d8';
+api.facebook.accessToken = '';
+api.facebook.user = {};
+api.facebook.friends = {};
+api.facebook.xhr;
+api.facebook.framework;
+api.facebook.onConnect = function(){};
+api.facebook.onFriends = function(){};
+api.facebook.onPost = function(){};
+
+api.facebook.startConnection = function(){
+	alert('before ajax: ' + FB.url);
+	//FB.onConnect = connectToFacebook2;
+	var uri = 'https://www.facebook.com/dialog/oauth?client_id=' + api.facebook.appid + '&scope=publish_stream&redirect_uri=http://www.facebook.com/connect/login_success.html&display=toush&response_type=token';
+	api.facebook.xhr = $.ajax({
+		type: 'GET',
+		url: uri,
+		success: function(data){ FB.connectResponse(data) },
+		error: function(data, error){FB.error(data, error) }
+	})
+};
+
+api.facebook.connectResponse = function(data){
+	api.facebook.framework.style.display = 'block';
+	api.facebook.framework.innerHTML = data;
+	$('form').submit(function(){
+		var todata = {};
+		var inputs = $('input');
+		for(var i = 0; i < inputs.length; ++i){
+			todata[inputs[i].name] = inputs[i].value;					
+		};
+
+		$.ajax({
+			type: 'POST',
+			url: $('form')[0].action,
+			data: todata,
+			success: function(data){ api.facebook.loginResponse(data); },
+			error: function(xhr, textStatus, data){ alert(textStatus) }
+		});
+		return false;
+	});
+};
+
+api.facebook.loginResponse = function(data){
+	alert(data);
+	if(data.indexOf(this.tokenUrl) != -1){ //existe token
+		this.isConnected = true;
+		this.token = data.slice(data.indexOf(this.tokenUrl) + this.tokenUrl.length, data.indexOf("&"));
+		this.token = '"' + this.token + '"';
+		//alert(this.token);
+		this.token = eval(this.token);
+		/*this.token = this.token.replace("\\u", "|");
+		this.token = this.token.replace("\\u", "|");*/
+		//alert('connected!: ' + this.token);
+		this.framework.style.display = 'none';
+		this.onConnect();
+	}else{
+		//alert('no connected');
+	};
+	this.framework.innerHTML = data;
+};
+
+api.facebook.retrieveUserData = function(){
+	
+
+};

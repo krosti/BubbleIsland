1fa58bc868f301d3759148697c7ec3827c7bef5a
jcapoduri <jcapoduri@gmail.com>
2011-05-28 02:25:48 -0300

    test

--- api.js
+++ api.js
@@ -506,8 +506,8 @@ api.softgame.confirmDoCoinsBuying = function(data, uri){
 	//parsear data en json
 	var getdata = api.string2JSON(data);
 	if(getdata.status == "SUCCESS"){
-		api.softgame.otoken = getdata.otoken;
-		alert('otoken: ' + api.softgame.otoken);
+		//api.softgame.otoken = getdata.otoken;
+		//alert('otoken: ' + api.softgame.otoken);
 		api.softgame.finalizeCoinsBuying(); 			
 	}else{
 		alert('Mmm.. we have a little inconvenient trying to reach you some lifes. Please can you try again and see if Hades wanna give you another chance?');
